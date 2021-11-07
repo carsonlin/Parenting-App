9748db857360019753831128555bb238ba77078e
Cuthbetr martin lwinga <clwinga@sfu.ca>
2021-11-07 00:14:30 -0700

    comment added

--- app/src/main/java/ca/cmpt276/chlorinefinalproject/MainActivity.java
+++ app/src/main/java/ca/cmpt276/chlorinefinalproject/MainActivity.java
@@ -18,15 +18,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         cardFace = findViewById(R.id.main_activity_card_face);
-
         coin coin = new coin(MainActivity.this,cardFace);
-
         cardFace.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 System.out.println("head? "+coin.flip().isHead());
-
             }
         });
 
