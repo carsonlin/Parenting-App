40ad18244d7d28a51e80028384cf6e49bef74935
Jack Manuel <jdmanuel@sfu.ca>
2021-11-06 18:08:15 -0700

    add setup button function to main activity
    need to insert the names of the coin, timer, and children activities

--- app/src/main/java/ca/cmpt276/chlorinefinalproject/MainActivity.java
+++ app/src/main/java/ca/cmpt276/chlorinefinalproject/MainActivity.java
@@ -2,7 +2,9 @@ package ca.cmpt276.chlorinefinalproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //setupButton(R.id.coinFlipButton, {name of coin flipping class}.class);
+        //setupButton(R.id.timerButton, {name of timer class}.class);
+        //setupButton(R.id.childrenConfigButton, {name of add children class}.class);
+    }
+
+    public void setupButton(int buttonId, Class<?> classToStart){
+        Button button = findViewById(buttonId);
+
+        button.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, classToStart);
+            startActivity(intent);
+        });
     }
 }
