b9685922490cae4a561456749d1013ddecf33a71
Jack Manuel <jdmanuel@sfu.ca>
2021-11-15 11:49:14 -0800

    setup task save button

--- app/src/main/java/ca/cmpt276/chlorinefinalproject/ViewTaskActivity.java
+++ app/src/main/java/ca/cmpt276/chlorinefinalproject/ViewTaskActivity.java
@@ -8,8 +8,12 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class ViewTaskActivity extends AppCompatActivity {
@@ -24,10 +28,24 @@ public class ViewTaskActivity extends AppCompatActivity {
         changeLayoutTextToTask();
         extractDataFromIntent();
         setupToolbar();
+        setupSaveButton();
+        setupEditText();
 
         // use taskIndex to get the right task from the taskManager
 
     }
+
+    private void setupSaveButton(){
+        Button saveButton = findViewById(R.id.saveButton);
+        saveButton.setOnClickListener(view -> {
+
+            EditText editText = findViewById(R.id.editChildName);
+            String taskName = editText.getText().toString();
+            Log.i("SAVE TASK", "Saving " + taskName);
+
+        });
+    }
+
 
     public static Intent makeIntent(Context context, int taskSelected){
         Intent intent = new Intent(context, ViewTaskActivity.class);
@@ -46,7 +64,12 @@ public class ViewTaskActivity extends AppCompatActivity {
         ActionBar ab = getSupportActionBar();
         assert ab != null;
         ab.setDisplayHomeAsUpEnabled(true);
-        ab.setTitle("Edit Task");
+        ab.setTitle(R.string.edit_task);
+    }
+
+    private void setupEditText() {
+        EditText editText = findViewById(R.id.editChildName);
+        editText.setInputType(InputType.TYPE_CLASS_TEXT);
     }
 
     private void changeLayoutTextToTask(){
@@ -54,8 +77,7 @@ public class ViewTaskActivity extends AppCompatActivity {
         textView.setText(R.string.task_name);
 
         textView = findViewById(R.id.deleteButton);
-        textView.setText(R.string.edit_task);
-
+        textView.setText(R.string.delete);
     }
 
     @Override
@@ -67,12 +89,12 @@ public class ViewTaskActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-
-        getMenuInflater().inflate(R.menu.menu_edit_task, menu);
-
-        return true;
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//
+//        getMenuInflater().inflate(R.menu.menu_edit_task, menu);
+//
+//        return true;
+//    }
 
 }
