82f3c3b25aa1fbbd567cd3c475e3ebab7468490f
carson <carson_lin@sfu.ca>
2021-11-22 15:40:23 -0800

    adding help screen and locking activities to portrait

---
+++ app/src/main/java/ca/cmpt276/chlorinefinalproject/HelpScreenActivity.java
@@ -0,0 +1,42 @@
+package ca.cmpt276.chlorinefinalproject;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
+
+import ca.cmpt276.chlorinefinalproject.databinding.ActivityHelpScreenBinding;
+
+public class HelpScreenActivity extends AppCompatActivity {
+    private ActivityHelpScreenBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help_screen);
+
+        setUpToolBar();
+        setupHyperLink();
+    }
+
+    private void setUpToolBar(){
+        binding = ActivityHelpScreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+        ActionBar ab = getSupportActionBar();
+        assert ab != null;
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void setupHyperLink() {
+        TextView citationLink = findViewById(R.id.citationLinks);
+        citationLink.setMovementMethod(LinkMovementMethod.getInstance());
+        citationLink.setLinkTextColor(Color.BLUE);
+    }
+
+
+}
